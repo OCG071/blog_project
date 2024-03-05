f15dc0e03155ef3afd522feadbc619eb478842c6
OCG071 <omarcg071@gmail.com>
2024-03-05 15:10:11 -0600

    First implementation of Django's added interface for the administrator

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
+@admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'authot', 'publish', 'status']
 # Register your models here.
