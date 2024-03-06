dec2c5e740cef3777b4b29dd6a2a44ceccb323bd
OCG071 <omarcg071@gmail.com>
2024-03-06 14:56:28 -0600

    Add routes to urls.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import Post
 
-@admin.site.register(Post)
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'authot', 'publish', 'status']
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
     list_filter = ['status', 'created', 'publish', 'autor']
     search_fields = ['title', 'body']
     prepopulated_fields = {'slug':('title',)}
