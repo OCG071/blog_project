8742780b4c45b11c12b1200c700bc96b795d0dac
OCG071 <omarcg071@gmail.com>
2024-03-12 15:53:20 -0600

    Working with the views on html.

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,10 @@ from django.http import Http404
 from .models import Post
 
 def post_list(request):
-    posts = Post.published.all()
+    posts = Post.publish.all()
     return render(request, 'blog/post/list.html',{'post':posts})
 
 def post_detail(request, id):
-    post = get_object_or_404(Post, id = id, status = Post.Status.PUBLISHED)
+    post = get_object_or_404(Post, id = id, status = Post.Status.PUBLISH)
     return render(request, 'blog/post/detail.html',{'post':post})
 # Create your views here.
