295fd40e7af150c4275974de231630c1ee52d26d
OCG071 <omarcg071@gmail.com>
2024-03-14 13:08:54 -0600

    Adding CSS style to the templates

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,12 @@ from django.http import Http404
 from .models import Post
 
 def post_list(request):
-    posts = Post.publish.all()
-    return render(request, 'blog/post/list.html',{'post':posts})
+    posts = Post.published.all()
+    print(posts)
+    return render(request, 'blog/post/list.html',{'posts':posts})
 
 def post_detail(request, id):
-    post = get_object_or_404(Post, id = id, status = Post.Status.PUBLISH)
+    post = get_object_or_404(Post, id = id, status = Post.Status.PUBLISHED)
+    print(post)
     return render(request, 'blog/post/detail.html',{'post':post})
 # Create your views here.
